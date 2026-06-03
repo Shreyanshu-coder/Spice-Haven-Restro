6c2328c8e5fe5ea55b2fa72dbeef516cf68da9f0
Shreyanshu Soni <shreyanshu189@gmail.com>
2026-06-03 11:13:52 +0530

    Updated resturant website design and functionality

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ button.addEventListener("click",function( ){
     console.log(navigation.classList);
 });
 
+const book_btn=document.querySelector(".book-btn");
+
+book_btn.addEventListener("click",function(){
+
+    const selectedSlot =
+        document.querySelector(".slot-btn.selected");
+
+    if(!selectedSlot){
+        alert("Please select a time slot.");
+        return;
+    }
+
+    alert(
+        `Table booked for ${selectedSlot.textContent}`
+    );
+});
+
+
+const slotButtons=document.querySelectorAll(".slot-btn");
+slotButtons.forEach(button=>{
+    button.addEventListener("click",()=>{
+
+        slotButtons.forEach(btn=>{
+            btn.classList.remove("selected");
+        });
+        button.classList.add("selected");
+    });
+});
